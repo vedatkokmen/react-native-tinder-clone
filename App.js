3e0a0deab179d1e46d63ec1d0cabe8b160924464
vedatkokmen <vedatkoekmen@gmail.com>
2021-11-12 02:23:19 +0300

    add assets folder

--- App.js
+++ App.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import {StyleSheet, Text, View, Image} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 const App = () => {
   return (
     <View>
-      <Image source={require('./assets/logo.png')} />
+      <Text></Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  image: {
-    width: 300,
-    height: 300,
-    borderRadius: 150,
-  },
-});
+const styles = StyleSheet.create({});
 export default App;
