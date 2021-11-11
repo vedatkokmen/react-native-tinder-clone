4e6f37c8117188e1e57b9e950666732b07a31fea
vedatkokmen <vedatkoekmen@gmail.com>
2021-11-12 02:36:12 +0300

    tinder card UI

--- App.js
+++ App.js
@@ -1,13 +1,78 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Image, ImageBackground} from 'react-native';
 
 const App = () => {
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.container}>
+      <View style={styles.card}>
+        <ImageBackground
+          style={styles.image}
+          source={{
+            uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png',
+          }}>
+          <View style={styles.info}>
+            <Text style={styles.name}>Elon</Text>
+            <Text style={styles.bio}>
+              A dude with a rocket is looking for a gal with fuel
+            </Text>
+          </View>
+        </ImageBackground>
+      </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  card: {
+    width: '95%',
+    height: '70%',
+    borderRadius: 10,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 10,
+    resizeMode: 'contain',
+  },
+  name: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  bio: {
+    fontSize: 18,
+    color: '#fff',
+    lineHeight: 25,
+  },
+  info: {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    height: '20%',
+    backgroundColor: 'rgba(0,0,0,0.1)',
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 10,
+  },
+});
+
 export default App;
